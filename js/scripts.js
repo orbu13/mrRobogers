1261497b6485c900f5d7eca31b1f70f58c1feb01
orbu13 <129992261+orbu13@users.noreply.github.com>
2023-06-29 22:40:39 -0700

    trying my if else, trying branching

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+console.log("loaded");
+
+function counting(userInput) {
+  for (let index = 0; index <= userInput; index++) {
+
+
+
+    if (index === 1) {
+      index = 'beep'
+    } else if (index === 2) {
+      index = 'boop'
+    } else if (index === 2) {
+      index = 'neighbor'
+    }
+
+    console.log(index);
+
+  }
+}
