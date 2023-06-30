68e9a590bd9021eec3704ee0d02f3100b008fad5
orbu13 <129992261+orbu13@users.noreply.github.com>
2023-06-29 22:04:53 -0700

    creating if else statement

--- mrRogers/js/scripts.js
+++ mrRogers/js/scripts.js
@@ -2,7 +2,19 @@ console.log("loaded");
 
 function counting(userInput) {
   for (let index = 0; index <= userInput; index++) {
-    console.log(index);
+    let result = "";
+
+    if (i = 1) {
+      result = "beep"
+    } else if (i = 2) {
+      result = "boop"
+    } else if (i = 3) {
+      result = "neiber"
+    } else {
+      result = 'else'
+    }
+
+    console.log(result);
 
   }
 }
