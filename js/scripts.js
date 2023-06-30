6ac0075c07c87433a1695b7c2326eb43d1361c52
orbu13 <129992261+orbu13@users.noreply.github.com>
2023-06-29 23:11:03 -0700

    trying different way, creating array

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,9 @@
 console.log("loaded");
+let arr = []
 
 function counting(userInput) {
   for (let index = 0; index <= userInput; index++) {
-
-
-
-    if (index === 1) {
-      index = 'beep'
-    } else if (index === 2) {
-      index = 'boop'
-    } else if (index === 2) {
-      index = 'neighbor'
-    }
-
-    console.log(index);
-
+    arr[index] = index;
   }
+  console.log(arr);
 }
